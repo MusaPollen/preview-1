2bc3872f1dd352cbe67341a9dadae8ccb4becca4
Musa <musa.pollen.light@gmail.com>
2025-09-16 16:06:40 +0600

    language tab, login form plus tab added

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,32 @@ import { Button } from '@heroui/button';
 
 
 import { Image } from "@heroui/image";
-import { Card, CardHeader, CardFooter, CardBody } from "@heroui/react";
+import { Card, CardHeader, CardFooter, CardBody, Tabs, Tab } from "@heroui/react";
+import { Form } from "@heroui/form";
+import React from "react";
+import { Input } from "@heroui/input";
+
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { EyeIcon } from '@heroicons/react/24/outline';
+import { EyeSlashIcon } from '@heroicons/react/24/outline';
+import { UserCircleIcon } from '@heroicons/react/24/outline';
+
+
 
 
 export default function Home() {
+
+  const [submitted, setSubmitted] = React.useState(null);
+  const [isVisible, setIsVisible] = React.useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+    console.log(data)
+    setSubmitted(data);
+  };
+
   return (
     <>
 
@@ -19,7 +41,7 @@ export default function Home() {
 
 
 
-
+      have to change logo size later
       {/* nav area */}
       <section className="flex flex-col sm:flex-row flex-wrap items-center justify-center p-4">
         {/* <div className="w-1/2 aspect-[4/1]"> */}
@@ -67,14 +89,85 @@ export default function Home() {
           />
         </Card>
 
+
+
+
+        {/* login form card */}
         <Card isFooterBlurred className="col-span-12 sm:col-span-5 row-span-2 bg-white/10 backdrop-blur-md" radius='none'>
-          {/* card header */}
-          <CardHeader className="flex gap-3 items-center justify-center">
-            <div className="flex flex-col">
+          <Tabs aria-label="Options" variant='underlined' color='primary' className="flex w-full flex-col items-end">
+            {/* ENGLISH */}
+            <Tab key="english" title="English" className="flex flex-col items-center">
               <p className="text-lg text-purple-600 font-bold">SIGN IN</p>
-            </div>
-          </CardHeader>
+              <Tabs aria-label="Options" size="lg" radius="full" color="secondary">
+                <Tab key="ibank" title="IBank">
+                  <Form onSubmit={onSubmit}>
+                    <div className="flex flex-col gap-y-4">
+                      <Input
+                        name="username"
+                        isRequired
+                        labelPlacement="outside"
+                        placeholder="Username"
+                        endContent={
+                          <UserCircleIcon className="w-6 h-6 text-purple-600" />
+                        }
+                      />
+                      <Input
+                        name="password"
+                        isRequired
+                        endContent={
+                          <button
+                            aria-label="toggle password visibility"
+                            className="focus:outline-solid outline-transparent"
+                            type="button"
+                            onClick={toggleVisibility}
+                          >
+                            {isVisible ? (
+                              <EyeIcon className="w-6 h-6 text-purple-600" />
+                            ) : (
+                              <EyeSlashIcon className="w-6 h-6 text-purple-600" />
+                            )}
+                          </button>
+                        }
+                        placeholder="Password"
+                        type={isVisible ? "text" : "password"}
+                      />
+                      <Button color="secondary" variant="ghost" type="submit">
+                        Submit
+                      </Button>
+                      {submitted && (
+                        <div className="text-small text-default-500">
+                          You submitted: <code>{JSON.stringify(submitted)}</code>
+                        </div>
+                      )}
+                    </div>
+                  </Form>
+                </Tab>
+                <Tab key="card" title="Card">
+                  <Card>
+                    <CardBody>
+                      Ut enim ad minim veniam, quis nostr
+                    </CardBody>
+                  </Card>
+                </Tab>
+              </Tabs>
+            </Tab>
+            {/* BANGLA+++++++++++++++++++++++++++++++++++++++++++++++++ */}
+            <Tab key="bangla" title="বাংলা" className="flex flex-col items-center">
+              <p className="text-lg text-purple-600 font-bold">সাইন ইন</p>
+            </Tab>
+          </Tabs>
+          {/* card header */}
         </Card>
+
+
+
+
+
+
+
+
+
+
 
         <Card className="col-span-12 sm:col-span-7 h-[200px]" radius='none'>
           <Image
