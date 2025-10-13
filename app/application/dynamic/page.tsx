9dd281c77744043c236434dc91ae1ef77ff52479
Musa <musa.pollen.light@gmail.com>
2025-10-13 11:52:13 +0600

    more vercel issues solved

--- app/application/dynamic/page.tsx
+++ app/application/dynamic/page.tsx
@@ -63,8 +63,8 @@ const sectionComponents: Record<string, (props: SectionProps) => React.ReactNode
 // ██   ██     ██   ████     ██████  
 
 export default function Home() {
-    let [currentStep, setCurrentStep] = useState(1);
-    let [totalSteps, setTotalSteps] = useState(4);
+    const [currentStep, setCurrentStep] = useState(1);
+    const [totalSteps, setTotalSteps] = useState(4);
     const [showAlert, setShowAlert] = useState(false);
     const [showSpinner, setShowSpinner] = useState(false);
     const [formData, setFormData] = useState({
@@ -404,7 +404,7 @@ export default function Home() {
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
                                             </svg>
 
-                                            Section "{sectionNamesCleaned[currentStep - 3] || '...'}", unavailabe...
+                                            Section {sectionNamesCleaned[currentStep - 3] || '...'}, unavailabe...
                                         </div>
                                     )
                                 }
